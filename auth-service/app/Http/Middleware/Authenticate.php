9019bb241ebee2dfca54efb4ae0a089412543476
Gilberto <gilberto@teknik.mx>
2024-12-29 14:40:49 -0600

    Protect Routes and Requests

--- auth-service/app/Http/Middleware/Authenticate.php
+++ auth-service/app/Http/Middleware/Authenticate.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class Authenticate extends Middleware
 {
@@ -12,6 +13,11 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        if (!$request->expectsJson()) {
+            abort(response()->json([
+                'message' => 'Unauthorized'
+            ], Response::HTTP_UNAUTHORIZED));
+        }
+        /* return $request->expectsJson() ? null : route('login'); */
     }
 }
--- auth-service/routes/api.php
+++ auth-service/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+ Route::prefix('v1')->middleware('api', 'jwt.verify')->group(function(){
+    Route::post('login', [AuthController::class,'login'])->name('login');
+
+    Route::post('register', [AuthController::class,'register']);
+    Route::post('logout', [AuthController::class,'logout']);
+    Route::post('refresh', [AuthController::class,'refresh']);
+    Route::get('me', [AuthController::class,'me']);
+    
 });
