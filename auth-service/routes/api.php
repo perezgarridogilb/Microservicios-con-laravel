11d2ca8b33cc4d0485a88705c063a2299bea331e
Gilberto <gilberto@teknik.mx>
2024-12-29 19:47:13 -0600

    Dockerize Microservice

--- auth-service/routes/api.php
+++ auth-service/routes/api.php
@@ -20,11 +20,12 @@ use Illuminate\Support\Facades\Route;
 // });
 
  Route::prefix('v1')->middleware('api', 'jwt.verify')->group(function(){
+    Route::prefix('auth')->group(function(){
     Route::post('login', [AuthController::class,'login'])->name('login');
 
     Route::post('register', [AuthController::class,'register']);
     Route::post('logout', [AuthController::class,'logout']);
     Route::post('refresh', [AuthController::class,'refresh']);
     Route::get('me', [AuthController::class,'me']);
-    
+});    
 });
